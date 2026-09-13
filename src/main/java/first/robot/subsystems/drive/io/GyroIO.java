9a2cf00e2ac64a698dfcbd844ccb9d71222ed7cc
Callan <beimelcm@gmail.com>
2026-09-12 21:29:53 -0400

    Started Work on issue 23. Created the gyroio file
    Kept the structure of moduleio file. while creating the gyroio file.

---
+++ src/main/java/first/robot/subsystems/drive/io/GyroIO.java
@@ -0,0 +1,31 @@
+package first.robot.subsystems.drive.io;
+
+import org.littletonrobotics.junction.AutoLog;
+import org.wpilib.math.geometry.Rotation2d;
+
+public interface GyroIO {
+    @AutoLog
+    public static class GyroIOInputs {
+        public boolean connected = false;
+        public Rotation2d yawPosition = new Rotation2d();
+        public double yawVelocityRadPerSec = 0.0;
+
+
+        public double[] odometryYawTimestamps = new double[]{};
+        public Rotation2d[] odometryYawPositions = new Rotation2d[]{};
+        
+    }
+
+    default void updateInputs(GyroIOInputs inputs) {}
+
+    //For a manual yaw zero or odometry reset when needed
+    default void setYaw(Rotation2d yaw) {}
+
+}
+
+
+
+
+
+
+
